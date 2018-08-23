359802ee8d98b983832f797cee85f43358a8bc2a
Sam Ashtar <samashtarm@gmail.com>
2018-08-23 05:08:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,4 @@
-	def dictionary
+def dictionary
     dictionary = {
         "hello" => "hi",
         "to" => "2",
@@ -8,10 +8,10 @@
         "four" => "4",
         "you" => "u",
         "at" => "@",
-        "and" => "&"
+        "and" => "&",
+        "be" => "b"
     }
 end
-
  def word_substituter(tweet)
     tweet_array = tweet.split.map do |word|
         if dictionary.keys.include?(word.downcase)
@@ -20,22 +20,26 @@ end
             word
         end
     end
-    puts tweet_array.join(" ")
+    tweet_array.join(" ")
 end
-
-
-def bulk_tweet_shortener(tweet_array)
+ #word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+ def bulk_tweet_shortener(tweet_array)
     tweet_array.each do |tweet|
         puts word_substituter(tweet)
     end
-  end
-
-
-def shortened_tweet_truncator(tweet)
+end
+ def selective_tweet_shortener(tweet)
+    if tweet.length > 140
+        word_substituter(tweet)
+    else
+        tweet
+    end
+end
+ def shortened_tweet_truncator(tweet)
     shortened = word_substituter(tweet)
     if shortened.length > 140
         shortened[0..134] + "(...)"
     else
-        shortened
+        tweet
     end
 end
